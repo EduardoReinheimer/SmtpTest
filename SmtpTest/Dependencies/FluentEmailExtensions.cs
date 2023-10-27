7a55e21a6afc8e6e7caef7101a6a0bc69ec7fe4e
Eduardo Salvador Reinheimer <du428@hotmail.com>
2023-10-27 13:07:43 -0300

    Ajustando Extension al archivo de configuraciones

--- SmtpTest/Dependencies/FluentEmailExtensions.cs
+++ SmtpTest/Dependencies/FluentEmailExtensions.cs
@@ -2,17 +2,33 @@
 
 public static class FluentEmailExtensions
 {
+    /// <summary>
+    /// Extensión creada para añadir un servicio de FluentEmail con la configuración de appsettings.json
+    /// </summary>
+    /// <param name="services">Servicios de tu API</param>
+    /// <param name="configuration">Configuraciones de tu API</param>
+    /// <param name="secureConnection">Si activo, tenéis que agregar user y password en appsettings.json</param>
+    /// <returns></returns>
     public static IServiceCollection AddFluentEmail(this IServiceCollection services,
-       IConfiguration configuration)
+       IConfiguration configuration, bool secureConnection = false)
     {
         var emailSettings = configuration.GetSection("EmailSettings");
         var defaultFromEmail = emailSettings["DefaultFromEmail"];
         var host = emailSettings["SMTPSetting:Host"];
-        var port = emailSettings.GetValue<int>("Port");
-        var userName = emailSettings["UserName"];
-        var password = emailSettings["Password"];
-        services.AddFluentEmail(defaultFromEmail)
-            .AddSmtpSender(host, port, userName, password);
+        var port = emailSettings.GetValue<int>("SMTPSetting:Port");
+        var userName = emailSettings?["SMTPSetting:UserName"];
+        var password = emailSettings?["SMTPSetting:Password"];
+
+        if (secureConnection)
+        {
+            services.AddFluentEmail(defaultFromEmail)
+                .AddSmtpSender(host, port, userName, password);
+        }
+        else
+        {
+            services.AddFluentEmail(defaultFromEmail)
+                .AddSmtpSender(host, port);
+        }
         return services;
     }
 }
